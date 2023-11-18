c9897a8984950f4fc0b14c31e326f9dca69a0319
shagib <shahelalam94@gmail.com>
2023-11-18 20:01:37 +0600

    team area style

--- js/custom.js
+++ js/custom.js
@@ -421,6 +421,52 @@
             ],
         });
 
+        // Team Area slider
+        $(".team-area-slider").slick({
+            slidesToShow: 6,
+            infinite: true,
+            autoplay: true,
+            draggable: true,
+            arrows: false,
+            slidesToScroll: 1,
+            loop: true,
+            dots: false,
+            speed: 1500,
+            rtl: false,
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 4,
+                    },
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 3,
+                    },
+                },
+                {
+                    breakpoint: 991,
+                    settings: {
+                        slidesToShow: 3,
+                    },
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 2,
+                    },
+                },
+                {
+                    breakpoint: 460,
+                    settings: {
+                        slidesToShow: 2,
+                    },
+                },
+            ],
+        });
+
         /*
         Counter Js
         ============================*/
